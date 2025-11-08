57bb09cd68fb5f483225f1edf41c8f24150b598e
Thomas Bonjoc <bonjocthomasagustin@gmail.com>
2025-11-08 16:38:14 +0800

    Fixed & Updated

--- raspi_code/main.py
+++ raspi_code/main.py
@@ -3,7 +3,7 @@ import cv2
 
 
 def main():
-    cap = cv2.VideoCapture("video/sample_video.mov")
+    cap = cv2.VideoCapture("video/chicken3.mp4")
     yolo_model = detection.model
     class_list = detection.class_list
 
@@ -15,7 +15,9 @@ def main():
             break
 
         boxes = detection.get_prediction_boxes(frame, yolo_model, confidence=0.25)
+        chicken_count = detection.count_all_chickens(boxes, class_list)
         with_boxes_frame = detection.detect_object(frame, boxes, class_list)
+        detection.display_chicken_count(frame, chicken_count)
 
         cv2.imshow("Chicken-Detection", with_boxes_frame) # diplay the frame or show frame
         if cv2.waitKey(1) & 0xFF == ord('q'):
