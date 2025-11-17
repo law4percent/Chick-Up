c1f13c0d71a82f1793cb2d0a6b0f2276bcd31b3d
Lawrence Roble <lawrence7roble@gmail.com>
2025-11-17 20:25:02 +0800

    Add auto create folders

--- raspi_code/main.py
+++ raspi_code/main.py
@@ -1,11 +1,20 @@
 from lib.processes import process_a, process_b, process_c
 from multiprocessing import Process, Queue, Event
 from lib.services import firebase_rtdb, handle_pairing
+import os
 
 def main(
-        testing_mode: bool = False,
-        show_logs: bool = False
+        testing_mode: bool  = False,
+        show_logs: bool     = False,
+        logs_path: str      = "logs",
+        yolo_path: str      = "YOLO"
     ) -> None:
+    if show_logs and not os.path.exists(logs_path):
+        os.makedirs(logs_path)
+        
+    if not os.path.exists(yolo_path):
+        os.makedirs(yolo_path)
+        
     firebase_rtdb.initialize_firebase(testing_mode=testing_mode, show_logs=show_logs)
     handle_pairing.pair_it(testing_mode=testing_mode, show_logs=show_logs)
 
@@ -49,5 +58,5 @@ def main(
 if __name__ == "__main__":
     main(
         testing_mode = True,
-        show_logs = True
+        show_logs = False
     )
