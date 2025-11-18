9fcc4fc314a564bc60780c8b4c5e9c8dfc242c9f
Lawrence Roble <lawrence7roble@gmail.com>
2025-11-19 01:34:53 +0800

    Remove unnecessary logs

--- raspi_code/lib/services/firebase_rtdb.py
+++ raspi_code/lib/services/firebase_rtdb.py
@@ -16,11 +16,6 @@ def initialize_firebase(
         save_logs: bool             = False
     ) -> None:
     service_acc_key_full_path = os.path.join(service_acc_key_path, file_name)
-    
-    print("Start initializing Firebase Credentials...")
-    if save_logs:
-        logging.info(f"Start initializing Firebase Credentials...")
-        
     if not os.path.exists(service_acc_key_full_path):
         print(f"Error: No such file {file_name} in {service_acc_key_path} folder.")
         if save_logs:
@@ -34,7 +29,3 @@ def initialize_firebase(
             'databaseURL': 'https://chick-up-1c2df-default-rtdb.asia-southeast1.firebasedatabase.app/'
         }
     )
-
-    if save_logs:
-        logging.info("Firebase initialized successfully.✅")
-    print("Firebase initialized successfully.✅")
