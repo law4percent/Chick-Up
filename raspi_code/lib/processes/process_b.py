ed359d37b74514b898c0e2d368f5b6e2d4c62bbf
Thomas Bonjoc <bonjocthomasagustin@gmail.com>
2025-11-19 12:59:06 +0800

    check process_b.py modified

--- raspi_code/lib/processes/process_b.py
+++ raspi_code/lib/processes/process_b.py
@@ -14,37 +14,49 @@ def process_B(
         number_of_class_instances: Queue,
         process_b_args: dict
     ) -> None:
+
     firebase_rtdb.initialize_firebase(save_logs=process_b_args["save_logs"])
-    print(f"{task_name}Running✅")
+    print(f"{task_name} Running ✅")
 
     user_uid = process_b_args["user_credentials"]["userUid"]
     linked_uid = process_b_args["user_credentials"]["linkedUid"]
+
     livestream_ref = db.reference(f"liveStream/{user_uid}/{linked_uid}")
+    detection_ref = db.reference(f"detection/{user_uid}/{linked_uid}")
 
     while True:
-        if not live_status.is_set(): # Control the live_status through process C, turn live_status on/off to start/stop streaming
-            print(f"{task_name}Live status is OFF. Waiting...")
+        if not live_status.is_set():
+            print(f"{task_name} Live status is OFF. Waiting...")
             time.sleep(0.5)
             continue
 
-        if queue_frame.empty():
-            print(f"{task_name}No frame in queue. Waiting...")
-            time.sleep(0.5)
-            continue
+        if not queue_frame.empty():
+            frame = queue_frame.get()
+            frame = np.array(frame, dtype=np.uint8)
+            
+            ret, buffer = cv2.imencode(".jpg", frame)
+            if not ret:
+                print("Error: Failed to encode frame.")
+            else:
+                jpg_text = base64.b64encode(buffer).decode("utf-8")
+                now = datetime.now()
+                lastUpdateAt = now.strftime('%m/%d/%Y at %H:%M:%S')
 
-        frame = queue_frame.get()
-        frame = np.array(frame, dtype=np.uint8)
-        
-        ret, buffer = cv2.imencode(".jpg", frame)
-        if not ret:
-            print("Error: Failed to encode frame.")
-            continue
+                livestream_ref.update({
+                    "base64": jpg_text,
+                    "lastUpdateAt": lastUpdateAt
+                })
 
-        jpg_text = base64.b64encode(buffer).decode("utf-8")
-        now = datetime.now()
-        lastUpdateAt = now.strftime('%m/%d/%Y at %H:%M:%S')
-        livestream_ref.update({
-            "base64": jpg_text,
-            "lastUpdateAt": lastUpdateAt
-        })
+        if not number_of_class_instances.empty():
+            class_counts = number_of_class_instances.get()
+            number_of_chickens = class_counts.get("chickens", 0)
+            number_of_intruders = class_counts.get("intruders", 0)
+
+            updatedAt = datetime.now().strftime('%m/%d/%Y %H:%M:%S')
+            detection_ref.update({
+                "numberOfChickens": number_of_chickens,
+                "numberOfIntruders": number_of_intruders,
+                "updatedAt": updatedAt
+            })
+
         time.sleep(0.5)
