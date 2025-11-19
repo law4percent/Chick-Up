41976d585895006652b9494fe9705e896035248f
law4percent <lawrence7roble@gmail.com>
2025-11-19 11:40:57 +0800

    Fix version of process B RTDB

--- raspi_code/lib/processes/process_b.py
+++ raspi_code/lib/processes/process_b.py
@@ -3,40 +3,48 @@ import cv2
 import base64
 import numpy as np
 from multiprocessing import Queue
-from lib.services.firebase_rtdb import db
+from lib.services import firebase_rtdb
+from firebase_admin import db
+from datetime import datetime
 
 def process_B(
         task_name: str,
         queue_frame: Queue,
         live_status: any,
-        live_uid: str = "kP718rjyRXWLDUupBhiQTRAaWKt2"
+        number_of_class_instances: Queue,
+        process_b_args: dict
     ) -> None:
-    print(f"{task_name} is starting...")
+    firebase_rtdb.initialize_firebase(save_logs=process_b_args["save_logs"])
+    print(f"{task_name}Running✅")
+
+    user_uid = process_b_args["user_credentials"]["userUid"]
+    linked_uid = process_b_args["user_credentials"]["linkedUid"]
+    livestream_ref = db.reference(f"liveStream/{user_uid}/{linked_uid}")
 
     while True:
-
-        if live_status.is_set():
+        if not live_status.is_set(): # Control the live_status through process C, turn live_status on/off to start/stop streaming
+            print(f"{task_name}Live status is OFF. Waiting...")
             time.sleep(0.5)
             continue
 
         if queue_frame.empty():
+            print(f"{task_name}No frame in queue. Waiting...")
             time.sleep(0.5)
             continue
 
-        frame_list = queue_frame.get()
-
-        frame = np.array(frame_list, dtype=np.uint8)
+        frame = queue_frame.get()
+        frame = np.array(frame, dtype=np.uint8)
         
         ret, buffer = cv2.imencode(".jpg", frame)
         if not ret:
             print("Error: Failed to encode frame.")
             continue
 
-
         jpg_text = base64.b64encode(buffer).decode("utf-8")
-
-        db.reference(f"live/{live_uid}/frameListFormat").set(jpg_text)
-
-        print(f"Uploaded frame to live/{live_uid}")
-
+        now = datetime.now()
+        lastUpdateAt = now.strftime('%m/%d/%Y at %H:%M:%S')
+        livestream_ref.update({
+            "base64": jpg_text,
+            "lastUpdateAt": lastUpdateAt
+        })
         time.sleep(0.5)
